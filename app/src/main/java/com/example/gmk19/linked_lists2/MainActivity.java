01f83384d3b0756d32863a6436ae1d6aa4dd1b8d
Grace <gmk1914@yahoo.com>
2018-02-12 22:07:21 -0600

    //

--- app/src/main/java/com/example/gmk19/linked_lists2/MainActivity.java
+++ app/src/main/java/com/example/gmk19/linked_lists2/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.gmk19.linked_lists2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -11,10 +13,12 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button front;
-    private Button end;
-    private ScrollView scrollview;
-    public Text inputNum;
+    private EditText newNumberET;
+    private EditText inputIndex;
+    //private Button addFrontButton;
+    //private Button addEndButton;
+    private ViewGroup linkedListContainer;
+    private LinkedList ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,38 +26,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinkedList ll = new LinkedList();
-        ll.addFront(2);
-        ll.addFront(5);
-        ll.addFront(7);
-        ll.addEnd(9);
-        ll.display();
+        this.newNumberET = (EditText)this.findViewById(R.id.newNumberET);
+        this.inputIndex = (EditText)this.findViewById(R.id.inputIndex);
+        //this.addFrontButton = (Button)this.findViewById(R.id.addFrontButton);
+        //this.addEndButton = (Button)this.findViewById(R.id.addEndButton);
+        this.linkedListContainer = (ViewGroup)this.findViewById(R.id.linkedListContainer);
 
-        this.front = (Button)this.findViewById(R.id.front);
-        this.end = (Button)this.findViewById(R.id.end);
-        this.scrollview = (ScrollView)this.findViewById(R.id.scrollview);
-        this.inputNum = (Text)this.findViewById(R.id.inputNum);
-
-
+        this.ll = new LinkedList(this.linkedListContainer, this);
+        this.ll.display();
     }
 
-    public void onButtonPressed(View v)
+
+
+    public void addFrontButtonClicked(View v)
     {
-        if(v == this.front)
+        String temp = this.newNumberET.getText().toString();
+        this.newNumberET.setText("");
+        this.ll.addFront(Integer.parseInt(temp));
+        //this.ll.display();
+    }
+
+    public void addEndButtonClicked(View v)
+    {
+        String temp = this.newNumberET.getText().toString();
+        this.newNumberET.setText("");
+        this.ll.addEnd(Integer.parseInt(temp));
+        //this.ll.display();
+    }
+
+    public void addAtIndexButtonClicked(View v)
+    {
+        String temp = this.inputIndex.getText().toString();
+        this.ll.addAtIndex(Integer.parseInt(temp));
+    }
+
+    public void removeAtIndexButtonClicked(View v)
+    {
+        String temp = this.inputIndex.getText().toString();
+        this.ll.removeAtIndex(Integer.parseInt(temp));
+    }
+
+    public void removeFrontButtonClicked(View v)
+    {
+        try
         {
-            ll.addFront(inputNum);
-            String enterNum = this.inputNum.getText().toString();
-            ScrollView scrollView = new ScrollView(this);
-            this.scrollview.addView(scrollView, 0);
-            scrollView(this.inputNum.getText().toString());
+            int value = this.ll.removeFront();
+            //do stuff with that element
+            this.ll.display();
         }
-        else
+        catch(Exception e)
         {
-            ll.addFront(inputNum);
-            String enterNum = this.inputNum.getText().toString();
-            ScrollView scrollView = new ScrollView(this);
-            this.scrollview.addView(scrollView, 0);
-            scrollView(this.inputNum.getText().toString());
+            System.err.println("Empty List");
         }
+    }
+
+    public void removeEndButtonClicked(View v)
+    {
+        try
+        {
+            int value = this.ll.removeEnd();
+            this.ll.display();
+        }
+        catch(Exception e){}
+
     }
 }
